affd3fc529700ccc86a97fed8b516d4a81bcbe6e
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2016-10-28 20:56:26 -0500

    Initial Commit

---
+++ Accediendo-a-la-nube-con-iOS_Semana-3_clima3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accediendo-a-la-nube-con-iOS_Semana-3_clima3
+//
+//  Created by Juan Carlos Carbajal Ipenza on 28/10/16.
+//  Copyright © 2016 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Accediendo-a-la-nube-con-iOS_Semana-3_clima3Tests/Accediendo_a_la_nube_con_iOS_Semana_3_clima3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Accediendo_a_la_nube_con_iOS_Semana_3_clima3Tests.swift
+//  Accediendo-a-la-nube-con-iOS_Semana-3_clima3Tests
+//
+//  Created by Juan Carlos Carbajal Ipenza on 28/10/16.
+//  Copyright © 2016 Juan Carlos Carbajal Ipenza. All rights reserved.
+//
+
+import XCTest
+@testable import Accediendo_a_la_nube_con_iOS_Semana_3_clima3
+
+class Accediendo_a_la_nube_con_iOS_Semana_3_clima3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
